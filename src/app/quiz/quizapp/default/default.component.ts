e9135b36ed1d260087982a25b0606d98d947f9de
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-01-30 06:43:25 +0000

    move quiz app to child component

---
+++ src/app/quiz/quizapp/default/default.component.ts
@@ -0,0 +1,68 @@
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import { RoadData } from '../road-signs';
+import { JsonPipe, NgForOf, NgIf, NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'app-default',
+  standalone: true,
+    imports: [
+        JsonPipe,
+        NgForOf,
+        NgIf,
+        NgOptimizedImage
+    ],
+  templateUrl: './default.component.html',
+  styleUrl: './default.component.css'
+})
+export class DefaultComponent implements OnInit{
+  mcqOptions: any[] = [];
+  currentQuestionIndex = 0;
+  imageSource: string = '';
+  altImgName: string = '';
+  question: string = '';
+
+  correctAnswer: any;  // Track the correct answer
+  score = 0;
+  categoryTracker = {};
+  quizContainer: any;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+  ngOnInit(): void {
+    this.startQuiz();
+  }
+
+  startQuiz(){
+
+    const roadData: RoadData = new RoadData();
+    const allSigns = roadData.getData();
+    const currentSign = allSigns[this.currentQuestionIndex];
+
+    // this.mcqOptions = [];
+
+    // Shuffle and select 3 random incorrect options
+    const incorrectOptions = this.shuffleArray(
+      allSigns.filter((sign: any) => sign.title !== currentSign.title) // Exclude current sign
+    ).slice(0, 3);
+
+    const finalOptions = this.shuffleArray([currentSign, ...incorrectOptions]);
+    this.mcqOptions = finalOptions.map((option:any) => option.title);
+    this.cdr.detectChanges();
+
+    this.currentQuestionIndex++;
+
+    this.question = "What does this sign mean?"
+    this.imageSource = currentSign.asset_source;
+    this.altImgName = currentSign.title;
+
+    console.log("Hello I am default mode");
+    if (!currentSign.asset_source) {
+      console.error("Invalid image source:", currentSign);
+    }
+    this.imageSource = currentSign.asset_source || 'assets/default.png'; // Fallback image
+  }
+
+  shuffleArray(array: any): any {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+}
--- src/app/quiz/quizapp/quizapp.component.ts
+++ src/app/quiz/quizapp/quizapp.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {RoadData} from './road-signs';
-import {NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
+import {JsonPipe, NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
+import {DefaultComponent} from './default/default.component';
 
 
 @Component({
@@ -10,91 +10,21 @@ import {NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
     NgIf,
     NgOptimizedImage,
     NgForOf,
+    JsonPipe,
+    DefaultComponent,
   ],
   templateUrl: './quizapp.component.html',
   styleUrl: './quizapp.component.css'
 })
 
 export class QuizComponent {
-  optionButtons: any = [];
 
-  mcqOptions: string[] = [];
-  mcqAnswer: string = '';
-
-
-  currentQuestionIndex = 0;
   isDefaultMode = true;
   isClicked = false;
-  imageSource: string = '';
-  altImgName: string = '';
-  question: string = '';
-  correctAnswer: any;  // Track the correct answer
-  score = 0;
-
-  categoryTracker = {};
-  private quizContainer: any;
 
   startQuiz(basicMode: boolean) {
     this.isDefaultMode = basicMode;
-    this.currentQuestionIndex = 0;
-
-    const roadData: RoadData = new RoadData();
-    const currentSign = roadData.getData();
-
-    if (this.isDefaultMode) this.defaultMode(currentSign);
-    else this.pictureMode(currentSign);
-
-    const shuffledSigns = this.shuffleArray(currentSign);
-
-    const options: any = [currentSign, ...shuffledSigns.filter((sign: any) => sign !== currentSign).slice(0, 3)];
-
-    const finalOptions = this.shuffleArray(options);
-
-    finalOptions.forEach((option: any) => {
-      const button = document.createElement('button');
-      this.mcqAnswer = this.isDefaultMode ? option.title : 'See Image';
-
-      console.log("String 1 is %s", option.title);
-      console.log("String 2 is %s", this.mcqAnswer);
-
-      // this.optionButtons.push(button);
-      do {
-        if (this.mcqAnswer != null || this.mcqAnswer != " " || this.mcqAnswer != undefined) {
-          this.mcqOptions.push(this.mcqAnswer);
-        }
-      } while (this.mcqOptions.length < 4);
-
-      console.log(this.mcqOptions);
-
-      for (let t = 0; t < this.mcqOptions.length; t++) {
-        console.log("Title is: %s", this.mcqOptions[t]);
-      }
-    });
-
-    this.currentQuestionIndex++;
     this.isClicked = true;
-  }
-
-  displayQuestion(): void {
-    // correctAnswer = currentSign; // Track the correct answer
-  }
-
-  defaultMode(array: any): void {
-    this.question = "What does this sign mean?"
-    this.imageSource = array[this.currentQuestionIndex].asset_source;
-    this.altImgName = array[this.currentQuestionIndex].title;
-
-    console.log("Hello I am default mode");
-  }
-
-  pictureMode(array: any): void {
-    this.altImgName = array[this.currentQuestionIndex].title;
-    this.question = `What does the sign titled '${this.altImgName}' look like?`
-    console.log("Hello I am picture mode");
-  }
-
-  shuffleArray(array: any): any {
-    return array.sort(() => Math.random() - 0.5);
   }
 
   test(): void {
