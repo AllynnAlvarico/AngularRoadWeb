10a387c2aef59020bea40829e9c08a9acd8d6b47
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-02-05 23:29:57 +0000

    added learn flash card feature: few bugs (images not showing)

--- src/app/learn/flash-card/flash-card.component.ts
+++ src/app/learn/flash-card/flash-card.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {RoadData} from '../../quiz/quizapp/road-signs';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-flash-card',
   standalone: true,
-    imports: [
-        RouterLink,
-        RouterLinkActive
-    ],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgForOf
+  ],
   templateUrl: './flash-card.component.html',
   styleUrl: './flash-card.component.css'
 })
-export class FlashCardComponent {
+export class FlashCardComponent implements OnInit {
+  roadData = new RoadData();
+  allSigns = this.roadData.getData();
+  currentIndex = 0;
+  imageSource: string = '';
+  imageName: string = '';
+  category: string = '';
+  ngOnInit(): void {
+    this.learn();
+  }
 
-  constructor(private sharedData: ShareData) {
+  learn(){
+    const currentSign = this.allSigns[this.currentIndex];
+
+    this.imageSource = currentSign.asset_source;
+    this.imageName = currentSign.title;
+    this.category = currentSign.title;
+  }
+  next(){
+    this.currentIndex++;
+    this.learn();
+  }
+  prev(){
+    this.currentIndex--;
+    this.learn();
   }
 }
