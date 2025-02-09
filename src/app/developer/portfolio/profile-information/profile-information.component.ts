59faf24c57432920d09f13d356fa3f8d0f295b92
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-02-09 18:48:18 +0000

    added stat section

--- src/app/developer/portfolio/profile-information/profile-information.component.ts
+++ src/app/developer/portfolio/profile-information/profile-information.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-profile-information',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './profile-information.component.html',
   styleUrl: './profile-information.component.css'
 })
